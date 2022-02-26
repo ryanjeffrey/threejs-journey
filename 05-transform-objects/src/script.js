37668a613ee4200c717cbf549315320d68b55c9a
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-02-26 15:46:54 -0600

    Created group for cube

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -10,28 +10,17 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
+const group = new THREE.Group()
+scene.add(group)
 
-
-// Position
-// mesh.position.x = 0.7
-// mesh.position.y = - 0.6
-// mesh.position.z = 1
-
-mesh.position.set(0.7, - 0.6, 1)
-
-scene.add(mesh)
-
-// Scale
-mesh.scale.set(2, 0.5, 0.5)
-
-// Rotation
-mesh.rotation.set(2, 1, 5)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1)
 
 // Axes helper
-const axesHelper = new THREE.AxesHelper(2)
+const axesHelper = new THREE.AxesHelper()
 scene.add(axesHelper)
 
 /**
@@ -49,7 +38,7 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
-camera.lookAt(mesh.position)
+// camera.lookAt()
 
 /**
  * Renderer
