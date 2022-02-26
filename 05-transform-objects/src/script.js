c56fce8747147aee452967bd55101ba0fc91fa7e
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-02-26 15:50:45 -0600

    Added 2 more cubes

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -19,6 +19,20 @@ const cube1 = new THREE.Mesh(
 )
 group.add(cube1)
 
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = - 2
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2;
+group.add(cube3);
+
 // Axes helper
 const axesHelper = new THREE.AxesHelper()
 scene.add(axesHelper)
