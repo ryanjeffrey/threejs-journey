48d99bf80e933505fd09dce190b7f0d6263c9dbc
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-02-26 12:59:24 -0600

    Added sizes and camera

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -6,4 +6,12 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
 // Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+scene.add(camera)
