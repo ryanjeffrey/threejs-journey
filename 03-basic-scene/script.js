d5de66965b1bdb1d177f03618a7e0f28e8e4ff9f
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-02-26 13:10:04 -0600

    Added renderer

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -1,3 +1,4 @@
+// Scene
 const scene = new THREE.Scene()
 
 // Red cube
@@ -15,3 +16,13 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 scene.add(camera)
+
+// Renderer
+const canvas = document.querySelector('.webgl')
+console.log(canvas)
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
