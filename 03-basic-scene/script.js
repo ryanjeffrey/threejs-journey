97fa74de40635d7bec19bf0364ee5af4a10faecb
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-02-26 12:52:49 -0600

    Added mesh

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -1,1 +1,9 @@
-console.log("Hello Three.js")
+const scene = new THREE.Scene()
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Camera
