241bf7c68944d82dffce8918c5abab58aa2d50a0
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-02-28 10:00:49 -0600

    Created gsap animation for cube

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -31,18 +31,21 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
-// Clock
-const clock = new THREE.Clock()
+// // Clock
+// const clock = new THREE.Clock()
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 })
 
 // Animations
 const tick = () => {
-    // Clock
-    const elapsedTime = clock.getElapsedTime()
+    // // Clock
+    // const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
-    camera.position.y = Math.sin(elapsedTime)
-    camera.position.x = Math.cos(elapsedTime)
-    camera.lookAt(mesh.position)
+    // // Update objects
+    // camera.position.y = Math.sin(elapsedTime)
+    // camera.position.x = Math.cos(elapsedTime)
+    // camera.lookAt(mesh.position)
 
     // Render
     renderer.render(scene, camera)
