910e9f3a33577cabc5d236d69918f28f1b8f6137
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-02-28 11:26:33 -0600

    Imported OrbitControls

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 // Cursor
 const cursor = {
@@ -48,6 +49,9 @@ camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+// Controls
+const controls = new OrbitControls(camera, canvas)
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -65,10 +69,10 @@ const tick = () =>
     // mesh.rotation.y = elapsedTime;
 
     // Update camera
-    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-    camera.position.y = cursor.y * 5
-    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-    camera.lookAt(mesh.position)
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 5
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.lookAt(mesh.position)
 
     // Render
     renderer.render(scene, camera)
