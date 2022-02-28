7d08062a0776bbd234f1512a60aaa5dafd45cc0b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-02-28 10:37:39 -0600

    Added Orthographic camera

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -24,7 +24,12 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+
+// Orthographic Camera
+const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.OrthographicCamera(- 1 * aspectRatio, 1 * aspectRatio, 1, - 1, 0.1, 100)
+
 camera.position.x = 2
 camera.position.y = 2
 camera.position.z = 2
