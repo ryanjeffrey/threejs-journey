ec150918d26a6ddc0f6397e3ddbd68c58036c49f
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-02 10:51:11 -0600

    Added for loop to create random triangles

--- 09-geometries/src/script.js
+++ 09-geometries/src/script.js
@@ -12,7 +12,20 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
+
+const geometry = new THREE.BufferGeometry()
+
+const count = 500
+const positionsArray = new Float32Array(count * 3 * 3)
+
+for(let i = 0; i < count * 3 * 3; i++){
+    positionsArray[i] = Math.random() - 0.5
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
+
 const material = new THREE.MeshBasicMaterial({ 
     color: 0xff0000,
     wireframe: true
