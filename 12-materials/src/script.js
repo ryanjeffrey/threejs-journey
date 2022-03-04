ac76ec8231362be889bc257e701e963a59dc7674
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-04 11:18:59 -0600

    Instantiated textureLoader

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -3,6 +3,11 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+
+/**
  * Base
  */
 // Canvas
