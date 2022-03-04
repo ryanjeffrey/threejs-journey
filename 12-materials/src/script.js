52fe1e0f334f56c416327872a5be9b0c305cba41
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-04 11:01:21 -0600

    Moved sphere away from plane

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -20,6 +20,7 @@ const sphere = new THREE.Mesh(
     new THREE.SphereBufferGeometry(0.5, 16, 16),
     material
 )
+sphere.position.x = - 1.5
 
 const plane = new THREE.Mesh(
     new THREE.PlaneBufferGeometry(1, 1),
