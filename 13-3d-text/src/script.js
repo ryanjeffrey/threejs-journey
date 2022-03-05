cb6b6c238cfc357d10c44a43b3ed91b2e288febe
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-05 11:20:08 -0600

    Instantiated font loader

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
 
 /**
  * Base
@@ -19,6 +20,17 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+
+/**
+ * Fonts
+ */
+const fontLoader = new FontLoader()
+
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json', () => {
+        console.log('font loaded')
+    }
+)
 
 /**
  * Object
