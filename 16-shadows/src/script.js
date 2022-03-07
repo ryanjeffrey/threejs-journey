14e4f569923ff2ec087d5c39ca28b410eb914548
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-07 13:18:02 -0600

    Enabled directionalLight to cast shadow

--- 16-shadows/src/script.js
+++ 16-shadows/src/script.js
@@ -32,6 +32,8 @@ gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(directionalLight)
 
+directionalLight.castShadow = true
+
 /**
  * Materials
  */
