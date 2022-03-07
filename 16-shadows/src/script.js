291086e05c3044b124cb0deef0373b9b4217fc3c
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-07 14:01:20 -0600

    Added point light to scene

--- 16-shadows/src/script.js
+++ 16-shadows/src/script.js
@@ -66,6 +66,14 @@ const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera)
 spotLightCameraHelper.visible = false
 scene.add(spotLightCameraHelper)
 
+// Point light
+const pointLight = new THREE.PointLight(0xffffff, 0.3)
+
+pointLight.castShadow = true
+
+pointLight.position.set(- 1, 1, 0)
+scene.add(pointLight)
+
 /**
  * Materials
  */
