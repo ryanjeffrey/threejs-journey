4c42b5421235bd8733318423dc1395ea49e741c7
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-07 14:09:38 -0600

    Optimized point light shadow

--- 16-shadows/src/script.js
+++ 16-shadows/src/script.js
@@ -70,6 +70,10 @@ scene.add(spotLightCameraHelper)
 const pointLight = new THREE.PointLight(0xffffff, 0.3)
 
 pointLight.castShadow = true
+pointLight.shadow.mapSize.width = 1024
+pointLight.shadow.mapSize.height = 1024
+pointLight.shadow.mapSize.near = 0.1
+pointLight.shadow.mapSize.far = 5
 
 pointLight.position.set(- 1, 1, 0)
 scene.add(pointLight)
