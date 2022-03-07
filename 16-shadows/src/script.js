0d4befb390844e3f7c1e760ded169208ced50b5f
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-07 13:54:41 -0600

    Changed camera near and far values to fit scene

--- 16-shadows/src/script.js
+++ 16-shadows/src/script.js
@@ -55,6 +55,8 @@ spotLight.castShadow = true
 spotLight.shadow.mapSize.width = 1024
 spotLight.shadow.mapSize.height = 1024
 spotLight.shadow.camera.fov = 30
+spotLight.shadow.camera.near = 1
+spotLight.shadow.camera.far = 6
 
 spotLight.position.set(0, 2, 2)
 scene.add(spotLight)
