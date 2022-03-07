925a925bbea5eee832674aba6bd505b1384003b4
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-07 10:15:54 -0600

    Set pointLight position

--- 15-lights/src/script.js
+++ 15-lights/src/script.js
@@ -31,6 +31,7 @@ const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
 scene.add(hemisphereLight)
 
 const pointLight = new THREE.PointLight(0xff9000, 0.5)
+pointLight.position.set(1, - 0.5, 1)
 scene.add(pointLight)
 
 /**
