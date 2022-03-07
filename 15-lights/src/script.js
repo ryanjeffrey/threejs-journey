1adcfc7f538ea600c66e4666c5265304316f830e
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-07 09:52:16 -0600

    Removed ambientLight and pointLight

--- 15-lights/src/script.js
+++ 15-lights/src/script.js
@@ -18,14 +18,7 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
 
 /**
  * Objects
