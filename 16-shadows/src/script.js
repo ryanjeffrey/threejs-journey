7ff89cb26eab51ea054b99e23b63b4bb3001bfde
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-07 13:51:15 -0600

    Increased resolution of spot light shadow

--- 16-shadows/src/script.js
+++ 16-shadows/src/script.js
@@ -19,12 +19,12 @@ const scene = new THREE.Scene()
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.4)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4)
 directionalLight.position.set(2, 2, - 1)
 gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
@@ -52,6 +52,8 @@ scene.add(directionalLightCameraHelper)
 const spotLight = new THREE.SpotLight(0xffffff, 0.4, 10, Math.PI * 0.3)
 
 spotLight.castShadow = true
+spotLight.shadow.mapSize.width = 1024
+spotLight.shadow.mapSize.height = 1024
 
 spotLight.position.set(0, 2, 2)
 scene.add(spotLight)
