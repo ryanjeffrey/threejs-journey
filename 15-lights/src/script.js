448079f72a9c1bccf9d03232e6e0c8fb2492214a
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-07 10:37:57 -0600

    Added spotLight to scene

--- 15-lights/src/script.js
+++ 15-lights/src/script.js
@@ -39,6 +39,10 @@ rectAreaLight.position.set(- 1.5, 0, 1.5)
 rectAreaLight.lookAt(new THREE.Vector3())
 scene.add(rectAreaLight)
 
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1)
+spotLight.position.set(0, 2, 3)
+scene.add(spotLight)
+
 /**
  * Objects
  */
