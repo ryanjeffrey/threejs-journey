63dba97311ab4d20e8af72cd98dd196f96387312
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-08 16:31:21 -0600

    Animate ghost2

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -268,6 +268,11 @@ const tick = () =>
     ghost1.position.y = Math.sin(elapsedTime * 3)
     ghost1.position.z = Math.sin(ghost1Angle) * 4
 
+    const ghost2Angle = - elapsedTime * 0.32
+    ghost2.position.x = Math.cos(ghost2Angle) * 5
+    ghost2.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
+    ghost2.position.z = Math.sin(ghost1Angle) * 5
+
 
     // Update controls
     controls.update()
