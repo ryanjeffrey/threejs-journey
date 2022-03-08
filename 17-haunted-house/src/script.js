cf82ae6a36068c60b3c8e433181d05e763c08a48
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-08 14:44:09 -0600

    Create the door with a plane

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -44,6 +44,15 @@ roof.rotation.y = Math.PI * 0.25
 roof.position.y = 3
 house.add(roof)
 
+// Door
+const door = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(2, 2),
+    new THREE.MeshStandardMaterial({ color: '#aa7b7b' })
+)
+door.position.y = 1
+door.position.z = 2 + 0.01
+house.add(door)
+
 // Floor
 const floor = new THREE.Mesh(
     new THREE.PlaneGeometry(20, 20),
