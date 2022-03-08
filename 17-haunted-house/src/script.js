3d851389f1b9d5df2d267b80d2661494face034d
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-08 16:46:34 -0600

    Optimize shadows

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -260,6 +260,7 @@ renderer.setClearColor('#262837')
  * Shadows
  */
 renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
 moonLight.castShadow = true
 doorLight.castShadow = true
@@ -274,6 +275,28 @@ bush3.castShadow = true
 bush4.castShadow = true
 
 floor.receiveShadow = true
+
+// Optimize shadows
+moonLight.shadow.mapSize.width = 256
+moonLight.shadow.mapSize.height = 256
+moonLight.shadow.camera.far = 15
+
+doorLight.shadow.mapSize.width = 256
+doorLight.shadow.mapSize.height = 256
+doorLight.shadow.camera.far = 7
+
+ghost1.shadow.mapSize.width = 256
+ghost1.shadow.mapSize.height = 256
+ghost1.shadow.camera.far = 7
+
+ghost2.shadow.mapSize.width = 256
+ghost2.shadow.mapSize.height = 256
+ghost2.shadow.camera.far = 7
+
+ghost3.shadow.mapSize.width = 256
+ghost3.shadow.mapSize.height = 256
+ghost3.shadow.camera.far = 7
+
 
 /**
  * Animate
