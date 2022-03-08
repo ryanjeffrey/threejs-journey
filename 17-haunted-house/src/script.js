9e27d84b5f6955e0bdfbf33de04c14b4558fc18a
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-08 16:15:09 -0600

    Apply grass textures to floor

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -148,8 +148,14 @@ for(let i = 0; i < 50; i++){
 // Floor
 const floor = new THREE.Mesh(
     new THREE.PlaneGeometry(20, 20),
-    new THREE.MeshStandardMaterial({ color: '#a9c388' })
+    new THREE.MeshStandardMaterial({ 
+        map: grassColorTexture,
+        aoMap: grassAmbientOcclusionTexture,
+        normalMap: grassNormalTexture,
+        roughnessMap: grassRoughnessTexture
+     })
 )
+floor.geometry.setAttribute('uv2', new THREE.Float32BufferAttribute(floor.geometry.attributes.uv.array, 2))
 floor.rotation.x = - Math.PI * 0.5
 floor.position.y = 0
 scene.add(floor)
