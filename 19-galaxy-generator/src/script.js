59cd435733a7e0c907956363375aa6318028afbd
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-09 13:42:16 -0600

    Increase width of debug panel

--- 19-galaxy-generator/src/script.js
+++ 19-galaxy-generator/src/script.js
@@ -7,7 +7,7 @@ import * as dat from 'lil-gui'
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI({ width: 400 })
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
