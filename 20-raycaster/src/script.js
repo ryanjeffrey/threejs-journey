68427ce9e2b79ff204d847327b9968d48c2c268b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-10 10:26:24 -0600

    Update sphere color to blue when mouse hovers objects

--- 20-raycaster/src/script.js
+++ 20-raycaster/src/script.js
@@ -74,8 +74,6 @@ const mouse = new THREE.Vector2()
 window.addEventListener('mousemove', (event) => {
     mouse.x = event.clientX / sizes.width * 2 - 1
     mouse.y = - (event.clientY / sizes.height) * 2 + 1
-
-    console.log(mouse)
 })
 
 /**
@@ -113,22 +111,18 @@ const tick = () =>
     object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
     object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
 
-    // // Cast a ray
-    // const rayOrigin = new THREE.Vector3(- 3, 0, 0)
-    // const rayDirection = new THREE.Vector3(1, 0, 0)
-    // rayDirection.normalize()
+    // Cast a ray
+    raycaster.setFromCamera(mouse, camera)
 
-    // raycaster.set(rayOrigin, rayDirection)
+    const objectsToTest = [object1, object2, object3]
+    const intersects = raycaster.intersectObjects(objectsToTest)
 
-    // const objectsToTest = [object1, object2, object3]
-    // const intersects = raycaster.intersectObjects(objectsToTest)
-
-    // for(const object of objectsToTest) {
-    //     object.material.color.set('#ff0000')
-    // }
-    // for(const intersect of intersects) {
-    //     intersect.object.material.color.set('#0000ff')
-    // }
+    for(const object of objectsToTest) {
+        object.material.color.set('#ff0000')
+    }
+    for(const intersect of intersects) {
+        intersect.object.material.color.set('#0000ff')
+    }
 
     // Update controls
     controls.update()
