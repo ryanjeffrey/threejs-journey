be6a9d69b24f6f0e2daef522468325684e355ffa
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-10 10:14:41 -0600

    Comment the code related to the raycaster in the tick function

--- 20-raycaster/src/script.js
+++ 20-raycaster/src/script.js
@@ -101,22 +101,22 @@ const tick = () =>
     object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
     object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
 
-    // Cast a ray
-    const rayOrigin = new THREE.Vector3(- 3, 0, 0)
-    const rayDirection = new THREE.Vector3(1, 0, 0)
-    rayDirection.normalize()
+    // // Cast a ray
+    // const rayOrigin = new THREE.Vector3(- 3, 0, 0)
+    // const rayDirection = new THREE.Vector3(1, 0, 0)
+    // rayDirection.normalize()
 
-    raycaster.set(rayOrigin, rayDirection)
+    // raycaster.set(rayOrigin, rayDirection)
 
-    const objectsToTest = [object1, object2, object3]
-    const intersects = raycaster.intersectObjects(objectsToTest)
+    // const objectsToTest = [object1, object2, object3]
+    // const intersects = raycaster.intersectObjects(objectsToTest)
 
-    for(const object of objectsToTest) {
-        object.material.color.set('#ff0000')
-    }
-    for(const intersect of intersects) {
-        intersect.object.material.color.set('#0000ff')
-    }
+    // for(const object of objectsToTest) {
+    //     object.material.color.set('#ff0000')
+    // }
+    // for(const intersect of intersects) {
+    //     intersect.object.material.color.set('#0000ff')
+    // }
 
     // Update controls
     controls.update()
