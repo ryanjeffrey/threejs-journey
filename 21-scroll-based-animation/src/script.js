31d30c39d49d267badb9210384fd873a2bc4d185
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-11 11:35:15 -0600

    Set magnification filter of the texture to NearestFilter

--- 21-scroll-based-animation/src/script.js
+++ 21-scroll-based-animation/src/script.js
@@ -32,6 +32,7 @@ const scene = new THREE.Scene()
 // Texture
 const textureLoader = new THREE.TextureLoader()
 const gradientTexture = textureLoader.load('textures/gradients/3.jpg')
+gradientTexture.magFilter = THREE.NearestFilter
 
 // Material
 const material = new THREE.MeshToonMaterial({ 
