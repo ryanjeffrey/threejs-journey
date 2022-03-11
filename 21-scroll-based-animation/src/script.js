f228e59593532103367813569441c8d9b6e67c4c
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-11 11:19:49 -0600

    Create three meshes and add them to scene

--- 21-scroll-based-animation/src/script.js
+++ 21-scroll-based-animation/src/script.js
@@ -24,6 +24,26 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Objects
+ */
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, 0.4, 16, 60),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 2, 32),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+
+scene.add(mesh1, mesh2, mesh3)
+
+/**
  * Sizes
  */
 const sizes = {
