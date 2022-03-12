55274ff947525f30f7b0d34acb95c5e1ccca0c82
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-12 10:37:37 -0600

    Set values so they go as far negative as positive (-0.5 to +0.5)

--- 21-scroll-based-animation/src/script.js
+++ 21-scroll-based-animation/src/script.js
@@ -134,8 +134,8 @@ cursor.x = 0
 cursor.y = 0
 
 window.addEventListener('mousemove', (event) => {
-    cursor.x = event.clientX / sizes.width
-    cursor.y = event.clientY / sizes.height
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = event.clientY / sizes.height - 0.5
 
     console.log(cursor)
 })
