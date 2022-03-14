2be50782f18839ea9ea5922c92788ac6b35267b4
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-14 13:47:35 -0500

    Add randomness to hit sound volume

--- 22-physics/src/script.js
+++ 22-physics/src/script.js
@@ -55,6 +55,7 @@ const playHitSound = (collision) => {
     const impactStrength = collision.contact.getImpactVelocityAlongNormal()
 
     if(impactStrength > 1.5) {
+        hitSound.volume = Math.random()
         hitSound.currentTime = 0
         hitSound.play()
     }
