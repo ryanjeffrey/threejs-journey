2742ea3d70e4f1fedd8ae21ddc30fea9f8ecefeb
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-14 11:59:07 -0500

    Create box

--- 22-physics/src/script.js
+++ 22-physics/src/script.js
@@ -158,12 +158,13 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const objectsToUpdate = []
 
+// Sphere
 const sphereGeometry = new THREE.SphereBufferGeometry(1, 20, 20)
 const sphereMaterial = new THREE.MeshStandardMaterial({
-            metalness: 0.3,
-            roughness: 0.4,
-            envMap: environmentMapTexture
-        })
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture
+})
 
 const createSphere = (radius, position) => {
     // Three.js mesh
@@ -193,6 +194,43 @@ const createSphere = (radius, position) => {
 }
 
 createSphere(0.5, { x: 0, y: 3, z: 0 })
+
+// Box
+const boxGeometry = new THREE.BoxBufferGeometry(1, 1, 1)
+const boxMaterial = new THREE.MeshStandardMaterial({
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture
+})
+
+const createBox = (width, height, depth, position) => {
+    // Three.js mesh
+    const mesh = new THREE.Mesh(boxGeometry, boxMaterial)
+    mesh.scale.set(width, height, depth)
+    mesh.castShadow = true
+    mesh.position.copy(position)
+    scene.add(mesh)
+
+    // Cannon.js body
+    const shape = new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5))
+
+    const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0, 3, 0),
+        shape: shape,
+        material: defaultMaterial
+    })
+    body.position.copy(position)
+    world.addBody(body)
+
+    // Save in objects to update
+    objectsToUpdate.push({
+        mesh,
+        body
+    })
+}
+
+createBox(1, 1.5, 2, { x: 0, y: 3, z: 0 })
 
 /**
  * Animate
