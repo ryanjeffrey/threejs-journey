c905a51088412ebdbea5563aebfda7a029b6120e
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-15 15:40:39 -0500

    Import GLTF Loader

--- 23-imported-models/src/script.js
+++ 23-imported-models/src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import * as dat from 'lil-gui'
 
 /**
