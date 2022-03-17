63993b5cebfa89f8d43bbd87c409bdd7bd1c0c23
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-17 10:31:57 -0500

    Set physicallyCorrectLights to true in renderer

--- 25-realistic-render/src/script.js
+++ 25-realistic-render/src/script.js
@@ -80,6 +80,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.physicallyCorrectLights = true
 
 /**
  * Animate
