68277f5984deaf24ade2215689acd3b0e78c8328
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-17 10:35:20 -0500

    Import and instantiate GLTFLoader

--- 25-realistic-render/src/script.js
+++ 25-realistic-render/src/script.js
@@ -2,6 +2,12 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+/**
+ * Loaders
+ */
+const gltfLoader = new GLTFLoader()
 
 /**
  * Base
