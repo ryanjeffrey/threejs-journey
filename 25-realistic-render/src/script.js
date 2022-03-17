c806ed98a567befe9d74d00758717438c944448c
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-17 10:53:52 -0500

    Load the Environment Map textures

--- 25-realistic-render/src/script.js
+++ 25-realistic-render/src/script.js
@@ -23,6 +23,18 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Environment map
+ */
+const environmentMap = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.jpg',
+    '/textures/environmentMaps/0/nx.jpg',
+    '/textures/environmentMaps/0/py.jpg',
+    '/textures/environmentMaps/0/ny.jpg',
+    '/textures/environmentMaps/0/pz.jpg',
+    '/textures/environmentMaps/0/nz.jpg'
+])
+
+/**
  * Models
  */
 gltfLoader.load(
