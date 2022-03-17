2567f94b1b98e76cab44690cfb32c78db710f3f8
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-17 11:54:25 -0500

    Update renderer to THREE.sRGBEncoding for more realism

--- 25-realistic-render/src/script.js
+++ 25-realistic-render/src/script.js
@@ -125,6 +125,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.physicallyCorrectLights = true
+renderer.outputEncoding = THREE.sRGBEncoding
 
 /**
  * Animate
