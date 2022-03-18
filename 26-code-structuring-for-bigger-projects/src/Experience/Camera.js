0b4d97c99f5fc69e2f03dae4ae0375804c2293b8
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-18 14:50:43 -0500

    Add OrbitControls to Camera class

--- 26-code-structuring-for-bigger-projects/src/Experience/Camera.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Camera.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import Experience from './Experience.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 export default class Camera {
     constructor(){
@@ -9,13 +10,17 @@ export default class Camera {
         this.canvas = this.experience.canvas
 
         this.setInstance()
+        this.setOrbitControls()
     }
 
     setInstance() {
         this.instance = new THREE.PerspectiveCamera(35, this.sizes.width / this.sizes.height, 0.1, 100)
         this.instance.position.set(6, 4, 8)
         this.scene.add(this.instance)
+    }
 
-        console.log(this.instance)
+    setOrbitControls() {
+        this.controls = new OrbitControls(this.instance, this.canvas)
+        this.controls.enableDamping = true
     }
 }
