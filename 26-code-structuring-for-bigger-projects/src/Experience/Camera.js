c49220a48b10227fb3486ee692f94040c323e379
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-18 14:44:38 -0500

    Create PerspectiveCamera and save it as an instance property

--- 26-code-structuring-for-bigger-projects/src/Experience/Camera.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Camera.js
@@ -1,3 +1,4 @@
+import * as THREE from 'three'
 import Experience from './Experience.js'
 
 export default class Camera {
@@ -6,5 +7,15 @@ export default class Camera {
         this.sizes = this.experience.sizes
         this.scene = this.experience.scene
         this.canvas = this.experience.canvas
+
+        this.setInstance()
+    }
+
+    setInstance() {
+        this.instance = new THREE.PerspectiveCamera(35, this.sizes.width / this.sizes.height, 0.1, 100)
+        this.instance.position.set(6, 4, 8)
+        this.scene.add(this.instance)
+
+        console.log(this.instance)
     }
 }
