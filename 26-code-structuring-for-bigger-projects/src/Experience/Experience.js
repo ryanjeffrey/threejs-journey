3c7858397d50e26a12c9ff90622eef4203cb6f19
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-18 14:24:39 -0500

    Instantiate Camera in the Experience class

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import Sizes from './Utils/Sizes.js'
 import Time from './Utils/Time.js'
+import Camera from './Camera.js'
 
 export default class Experience {
     constructor(canvas) {
@@ -14,6 +15,7 @@ export default class Experience {
         this.sizes = new Sizes()
         this.time = new Time()
         this.scene = new THREE.Scene()
+        this.camera = new Camera()
 
         // Sizes resize event
         this.sizes.on('resize', () => {
