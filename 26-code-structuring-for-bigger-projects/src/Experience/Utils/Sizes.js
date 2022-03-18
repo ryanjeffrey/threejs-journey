25eaee30864d47f8bb5f5361d1c75f07c7496625
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-18 13:20:51 -0500

    Listen to the resize event and update those properties

--- 26-code-structuring-for-bigger-projects/src/Experience/Utils/Sizes.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Utils/Sizes.js
@@ -4,5 +4,14 @@ export default class Sizes {
         this.width = window.innerWidth
         this.height = window.innerHeight
         this.pixelRatio = Math.min(window.devicePixelRatio, 2)
+
+        // Resize event
+        window.addEventListener('resize', () => {
+            this.width = window.innerWidth
+            this.height = window.innerHeight
+            this.pixelRatio = Math.min(window.devicePixelRatio, 2)
+
+            console.log(this)
+        })
     }
 }
