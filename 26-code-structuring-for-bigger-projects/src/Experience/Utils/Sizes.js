b1f055d47088bc44493aa182cc95fc8aa7e80922
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-18 13:30:19 -0500

    Make the Sizes class inherit from the EventEmitter

--- 26-code-structuring-for-bigger-projects/src/Experience/Utils/Sizes.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Utils/Sizes.js
@@ -1,5 +1,9 @@
-export default class Sizes {
+import EventEmitter from './EventEmitter'
+
+export default class Sizes extends EventEmitter {
     constructor() {
+        super()
+
         // Setup
         this.width = window.innerWidth
         this.height = window.innerHeight
