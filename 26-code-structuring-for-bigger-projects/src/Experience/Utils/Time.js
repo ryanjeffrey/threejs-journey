0f99d36fdef98204773b628e46c2b2373e245c98
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-18 14:00:53 -0500

    Update current time, calculate delta time and update elapsed time

--- 26-code-structuring-for-bigger-projects/src/Experience/Utils/Time.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Utils/Time.js
@@ -16,6 +16,11 @@ export default class Time extends EventEmitter {
     }
 
     tick() {
+        const currentTime = Date.now()
+        this.delta = currentTime - this.current
+        this.current = currentTime
+        this.elapsed = this.current - this.start
+        
         window.requestAnimationFrame(() => {
             this.tick()
         })
