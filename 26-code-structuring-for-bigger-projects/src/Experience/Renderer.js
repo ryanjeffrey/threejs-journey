8b2bae60e83d161093a6c829bd732aec5d755789
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-18 15:07:21 -0500

    Create a Renderer class

---
+++ 26-code-structuring-for-bigger-projects/src/Experience/Renderer.js
@@ -0,0 +1,11 @@
+import Experience from './Experience.js'
+
+export default class Renderer {
+    constructor() {
+        this.experience = new Experience()
+        this.canvas = this.experience.canvas
+        this.sizes = this.experience.sizes
+        this.scene = this.experience.scene
+        this.camera = this.experience.camera
+    }
+}
