0d4898125a8fbeb9a3ce29d9b733cfa13bad1683
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-18 15:10:27 -0500

    Import and instantiate Renderer

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import Sizes from './Utils/Sizes.js'
 import Time from './Utils/Time.js'
 import Camera from './Camera.js'
+import Renderer from './Renderer.js'
 
 let instance = null
 
@@ -24,6 +25,7 @@ export default class Experience {
         this.time = new Time()
         this.scene = new THREE.Scene()
         this.camera = new Camera()
+        this.renderer = new Renderer()
 
         // Sizes resize event
         this.sizes.on('resize', () => {
