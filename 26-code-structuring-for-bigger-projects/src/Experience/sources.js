73c6856e7eece4ff199e9303280f4b4c0beece15
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-19 13:36:01 -0500

    Load foxModel

--- 26-code-structuring-for-bigger-projects/src/Experience/sources.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/sources.js
@@ -21,5 +21,10 @@ export default [
         name: 'grassNormalTexture',
         type: 'texture',
         path: 'textures/dirt/normal.jpg'
+    },
+    {
+        name: 'foxModel',
+        type: 'gltfModel',
+        path: 'models/Fox/glTF/Fox.gltf'
     }
 ]
