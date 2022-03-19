5591828434ca6cc46a67f048a18569ae9ae63987
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-19 15:15:55 -0500

    Traverse the scene to look for anything to dispose

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -59,5 +59,24 @@ export default class Experience {
     destroy() {
         this.sizes.off('resize')
         this.time.off('tick')
+
+        // Traverse the whole scene
+        this.scene.traverse((child) => {
+
+            // Test if it's a mesh
+            if(child instanceof THREE.Mesh) {
+                child.geometry.dispose()
+
+                // Loop through the material properties
+                for(const key in child.material) {
+                    const value = child.material[key]
+
+                    // Test if there is a dispose function
+                    if(value && typeof value.dispose === 'function') {
+                        value.dispose()
+                    }
+                }
+            }
+        })
     }
 }
