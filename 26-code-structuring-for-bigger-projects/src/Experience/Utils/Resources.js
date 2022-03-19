ab1a3d8bbfcfd23d94b82865c3009b20c8bf2a02
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-19 10:28:14 -0500

    Save sources as a property in the Resources class

--- 26-code-structuring-for-bigger-projects/src/Experience/Utils/Resources.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Utils/Resources.js
@@ -1,7 +1,12 @@
 import EventEmitter from './EventEmitter.js'
 
 export default class Resources extends EventEmitter {
-    constructor() {
+    constructor(sources) {
         super()
+
+        // Options
+        this.sources = sources
+
+        console.log(this.sources)
     }
 }
