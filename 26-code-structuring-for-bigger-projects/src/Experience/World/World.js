e4bb700b40267719ae0a6544f5ba821cc3a2b6f0
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-19 13:33:53 -0500

    Remove test cube from scene

--- 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
@@ -9,13 +9,6 @@ export default class World {
         this.scene = this.experience.scene
         this.resources = this.experience.resources
 
-        // Test mesh
-        const testMesh = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshStandardMaterial()
-        )
-        this.scene.add(testMesh)
-
         // Wait for resources
         this.resources.on('ready', () => {
             // Setup
