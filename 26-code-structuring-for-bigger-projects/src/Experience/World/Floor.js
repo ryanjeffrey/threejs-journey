e406019c60dda0d3727ad8d5cc3243ae58dbbf7f
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-19 13:29:05 -0500

    Add the mesh with shadow support

--- 26-code-structuring-for-bigger-projects/src/Experience/World/Floor.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/Floor.js
@@ -40,6 +40,9 @@ export default class Floor {
     }
 
     setMesh() {
-
+        this.mesh = new THREE.Mesh(this.geometry, this.material)
+        this.mesh.rotation.x = - Math.PI * 0.5
+        this.mesh.receiveShadow = true
+        this.scene.add(this.mesh)
     }
 }
