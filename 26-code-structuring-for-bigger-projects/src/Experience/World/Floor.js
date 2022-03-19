03614c8b095339811e21a21def6bed551aebc067
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-19 11:57:48 -0500

    Add texture and normal

--- 26-code-structuring-for-bigger-projects/src/Experience/World/Floor.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/Floor.js
@@ -18,7 +18,18 @@ export default class Floor {
     }
 
     setTextures() {
+        this.textures = {}
 
+        this.textures.color = this.resources.items.grassColorTexture
+        this.textures.color.encoding = THREE.sRGBEncoding
+        this.textures.color.repeat.set(1.5, 1.5)
+        this.textures.color.wrapS = THREE.RepeatWrapping
+        this.textures.color.wrapT = THREE.RepeatWrapping
+
+        this.textures.normal = this.resources.items.grassNormalTexture
+        this.textures.normal.repeat.set(1.5, 1.5)
+        this.textures.normal.wrapS = THREE.RepeatWrapping
+        this.textures.normal.wrapT = THREE.RepeatWrapping
     }
 
     setMaterial() {
