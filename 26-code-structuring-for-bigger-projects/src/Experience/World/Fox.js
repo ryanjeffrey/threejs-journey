8a859ef44b03fd005a0c96829d8cc44efbb8bc29
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-19 13:38:53 -0500

    Create Fox class

---
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/Fox.js
@@ -0,0 +1,9 @@
+import Experience from '../Experience.js'
+
+export default class Fox {
+    constructor() {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+    }
+}
