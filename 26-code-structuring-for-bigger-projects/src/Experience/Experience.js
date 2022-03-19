fe10c646149255438e0a38980caa28308c1c2f57
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-19 10:16:01 -0500

    Import and instantiate Resources class

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -4,6 +4,7 @@ import Time from './Utils/Time.js'
 import Camera from './Camera.js'
 import Renderer from './Renderer.js'
 import World from './World/World.js'
+import Resources from './Utils/Resources.js'
 
 let instance = null
 
@@ -25,6 +26,7 @@ export default class Experience {
         this.sizes = new Sizes()
         this.time = new Time()
         this.scene = new THREE.Scene()
+        this.resources = new Resources()
         this.camera = new Camera()
         this.renderer = new Renderer()
         this.world = new World()
