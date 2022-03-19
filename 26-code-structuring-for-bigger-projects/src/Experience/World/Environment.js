9cd723fc0bfc6b910447dc543a1d53e327e980b4
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-19 11:22:21 -0500

    Retrieve the Resources instance and use environmentMapTexture to create the environment map

--- 26-code-structuring-for-bigger-projects/src/Experience/World/Environment.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/Environment.js
@@ -5,9 +5,11 @@ export default class Environment {
     constructor() {
         this.experience = new Experience()
         this.scene = this.experience.scene
+        this.resources = this.experience.resources
 
         // Setup
         this.setSunLight()
+        this.setEnvironmentMap()
     }
 
     setSunLight() {
@@ -18,5 +20,14 @@ export default class Environment {
         this.sunLight.shadow.normalBias = 0.05
         this.sunLight.position.set(3.5, 2, -1.25)
         this.scene.add(this.sunLight)
+    }
+
+    setEnvironmentMap() {
+        this.environmentMap = {}
+        this.environmentMap.intensity = 0.4
+        this.environmentMap.texture = this.resources.items.environmentMapTexture
+        this.environmentMap.texture.encoding = THREE.sRGBEncoding
+
+        this.scene.environment = this.environmentMap.texture
     }
 }
