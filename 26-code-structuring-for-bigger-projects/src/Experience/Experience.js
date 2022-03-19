eb6569c663ad769382bc3444791e3e598b6c61e8
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-19 15:22:04 -0500

    Dispose of debug ui

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -81,5 +81,9 @@ export default class Experience {
 
         this.camera.controls.dispose()
         this.renderer.instance.dispose()
+
+        if(this.debug.active) {
+            this.debug.ui.destroy()
+        }
     }
 }
