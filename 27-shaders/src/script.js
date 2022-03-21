a4dcb8242be4e5f3fe3a65eff987f1fdbff606e4
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-21 16:22:37 -0500

    Add a uColor uniform and use the Color class

--- 27-shaders/src/script.js
+++ 27-shaders/src/script.js
@@ -46,7 +46,8 @@ const material = new THREE.RawShaderMaterial({
     uniforms:
     {
         uFrequency: { value: new THREE.Vector2(10, 5) },
-        uTime: { value: 0 }
+        uTime: { value: 0 },
+        uColor: { value: new THREE.Color('orange') }
     }
 })
 
