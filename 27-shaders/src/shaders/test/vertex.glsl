bc4b205d37e4645a8af079353ff6d1a72ae2ca8e
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-21 14:58:14 -0500

    Create a vRandom varying and update it in the main function

--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -5,6 +5,8 @@ uniform mat4 modelMatrix;
 attribute vec3 position;
 attribute float aRandom;
 
+varying float vRandom;
+
 void main()
 {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
@@ -14,4 +16,6 @@ void main()
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectedPosition;
+
+    vRandom = aRandom;
 }
