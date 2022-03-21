dcafce63123b3c8496b3c33d0fdf0259224cf64b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-21 16:00:48 -0500

    Retrieve the value in the vertex shader and use it for the waves frequency

--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -1,13 +1,14 @@
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
+uniform float uFrequency;
 
 attribute vec3 position;
 
 void main()
 {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+    modelPosition.z += sin(modelPosition.x * uFrequency) * 0.1;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
