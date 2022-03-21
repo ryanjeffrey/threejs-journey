75c07f47c763a1633a0f1fb39209c1a3e8ca234b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-21 14:42:00 -0500

    Extend code for more precise control

--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -6,5 +6,10 @@ attribute vec3 position;
 
 void main()
 {
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
 }
