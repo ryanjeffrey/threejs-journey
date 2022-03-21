3f511eaa655d155de1d9fbd87fb845d894f5fe54
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-21 14:43:23 -0500

    Create wave effect on plane

--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -7,6 +7,7 @@ attribute vec3 position;
 void main()
 {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
