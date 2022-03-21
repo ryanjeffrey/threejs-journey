3367de3f3baa4215e3c6d7c98de8040c063e662f
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-21 16:41:56 -0500

    Retrieve uv coordinates and create a varying for them

--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -5,6 +5,9 @@ uniform vec2 uFrequency;
 uniform float uTime;
 
 attribute vec3 position;
+attribute vec2 uv;
+
+varying vec2 vUv;
 
 void main()
 {
@@ -16,4 +19,6 @@ void main()
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectedPosition;
+
+    vUv = uv;
 }
