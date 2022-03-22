ee33c3c7199808189d2c76bddaf110f181ade5ce
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-22 15:29:24 -0500

    Create sharp gradient

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -8,8 +8,11 @@ void main()
     // // Pattern 4
     // float strength = vUv.y;
 
-    // Pattern 5
-    float strength = 1.0 - vUv.y;
+    // // Pattern 5
+    // float strength = 1.0 - vUv.y;
+
+    // Pattern 6
+    float strength = vUv.y * 10.0;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
