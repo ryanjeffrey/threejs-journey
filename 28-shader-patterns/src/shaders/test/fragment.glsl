59951f7c3ef37522d0d2355bf65b9307f33f1893
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-22 15:27:06 -0500

    Flip gradient

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -5,8 +5,11 @@ void main()
     // // Pattern 3
     // float strength = vUv.x;
 
-    // Pattern 4
-    float strength = vUv.y;
+    // // Pattern 4
+    // float strength = vUv.y;
+
+    // Pattern 5
+    float strength = 1.0 - vUv.y;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
