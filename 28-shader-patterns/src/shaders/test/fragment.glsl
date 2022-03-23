efd39b16d37d1543db1217180ee5a947ebda0bf0
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 11:08:18 -0500

    Add step to wave effect

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -248,8 +248,11 @@ void main()
     // // Pattern 48
     // float strength = 1.0 - abs(cnoise(vUv * 10.0));
 
-    // Pattern 49
-    float strength = sin(cnoise(vUv * 10.0) * 20.0);
+    // // Pattern 49
+    // float strength = sin(cnoise(vUv * 10.0) * 20.0);
+
+    // Pattern 50
+    float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
