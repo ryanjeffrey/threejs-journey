c3f5edb99d24df30a388d69a27b2a8c81e7de562
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 11:03:22 -0500

    Add step to perlin noise

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -239,8 +239,11 @@ void main()
     // float radius = 0.25 + sin(angle * 100.0) * 0.02;
     // float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - radius));
 
-    // Pattern 46
-    float strength = cnoise(vUv * 10.0);
+    // // Pattern 46
+    // float strength = cnoise(vUv * 10.0);
+
+    // Pattern 47
+    float strength = step(0.0, cnoise(vUv * 10.0));
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
