69b9dee949f37aa6d840c23cc8b451ae23489d19
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 09:46:42 -0500

    Draw star-ish diamond

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -121,8 +121,12 @@ void main()
     // // Pattern 29
     // float strength = 0.015 / (distance(vUv, vec2(0.5)));
 
-    // Pattern 30
+    // // Pattern 30
+    // float strength = 0.15 / (distance(vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
+
+    // Pattern 31
     float strength = 0.15 / (distance(vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
+    strength *= 0.15 / (distance(vec2(vUv.y, (vUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
