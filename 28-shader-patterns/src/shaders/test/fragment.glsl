71f24ffd2d2b5e07bac3367ccf1b68333a891541
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 10:35:05 -0500

    Add offset to vUv to move angle to center

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -168,8 +168,12 @@ void main()
     // vec2 wavedUv = vec2(vUv.x + sin(vUv.y * 100.0) * 0.1, vUv.y + sin(vUv.x * 100.0) * 0.1);
     // float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
 
-    // Pattern 40
-    float angle = atan(vUv.x, vUv.y);
+    // // Pattern 40
+    // float angle = atan(vUv.x, vUv.y);
+    // float strength = angle;
+
+    // Pattern 41
+    float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
     float strength = angle;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
