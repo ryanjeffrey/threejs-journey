7658f19a855ad99c5ab8f22cb78fcfd1f2e7cfa3
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 10:22:17 -0500

    Combine previous patterns to get a circle with stroke

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -147,8 +147,11 @@ void main()
     // // Pattern 33
     // float strength = step(0.5, distance(vUv, vec2(0.5)) + 0.25);
 
-    // Pattern 34
-    float strength = abs(distance(vUv, vec2(0.5)) - 0.25);
+    // // Pattern 34
+    // float strength = abs(distance(vUv, vec2(0.5)) - 0.25);
+
+    // Pattern 35
+    float strength = step(0.02, abs(distance(vUv, vec2(0.5)) - 0.25));
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
