3d26fb9e337930a1c6d9bfd979ac1ee698f5f1e0
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 14:09:46 -0500

    Add uBigWavesSpeed tweak to debug UI

--- 29-raging-sea/src/script.js
+++ 29-raging-sea/src/script.js
@@ -40,7 +40,7 @@ const waterMaterial = new THREE.ShaderMaterial({
 gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value').min(0).max(1).step(0.001).name('uBigWavesElevation')
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x').min(0).max(10).step(0.001).name('uBigWavesFrequencyX')
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y').min(0).max(10).step(0.001).name('uBigWavesFrequencyZ')
-
+gui.add(waterMaterial.uniforms.uBigWavesSpeed, 'value').min(0).max(4).step(0.001).name('uBigWavesSpeed')
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
