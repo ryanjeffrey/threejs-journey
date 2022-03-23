25d1fb537e6ac30aa42326b11c7be481477fa534
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 13:02:14 -0500

    Move position of y with sine wave based on x

--- 29-raging-sea/src/shaders/water/vertex.glsl
+++ 29-raging-sea/src/shaders/water/vertex.glsl
@@ -1,6 +1,8 @@
 void main()
 {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    modelPosition.y += sin(modelPosition.x);
+
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
