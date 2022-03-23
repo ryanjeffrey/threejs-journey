239a62492e095922b69262e202c9776a7c7f9a25
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 16:04:31 -0500

    Add the uniforms to debug UI

--- 29-raging-sea/src/script.js
+++ 29-raging-sea/src/script.js
@@ -57,6 +57,10 @@ gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value').min(0).max(1).step(0
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x').min(0).max(10).step(0.001).name('uBigWavesFrequencyX')
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y').min(0).max(10).step(0.001).name('uBigWavesFrequencyZ')
 gui.add(waterMaterial.uniforms.uBigWavesSpeed, 'value').min(0).max(4).step(0.001).name('uBigWavesSpeed')
+gui.add(waterMaterial.uniforms.uSmallWavesElevation, 'value').min(0).max(1).step(0.001).name('uSmallWavesElevation')
+gui.add(waterMaterial.uniforms.uSmallWavesFrequency, 'value').min(0).max(30).step(0.001).name('uSmallWavesFrequency')
+gui.add(waterMaterial.uniforms.uSmallWavesSpeed, 'value').min(0).max(4).step(0.001).name('uSmallWavesSpeed')
+gui.add(waterMaterial.uniforms.uSmallWavesIterations, 'value').min(0).max(5).step(1).name('uSmallWavesIterations')
 gui.addColor(debugObject, 'depthColor').onChange(() => {
   waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor)
 })
