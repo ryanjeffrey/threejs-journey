95973ecbf91d41c69d8b352447d98350647cec09
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 13:18:50 -0500

    Retrieve the uniform and apply it to the elevation

--- 29-raging-sea/src/shaders/water/vertex.glsl
+++ 29-raging-sea/src/shaders/water/vertex.glsl
@@ -1,11 +1,12 @@
 uniform float uBigWavesElevation;
+uniform vec2 uBigWavesFrequency;
 
 void main()
 {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
     // Elevation
-    float elevation = sin(modelPosition.x) * uBigWavesElevation;
+    float elevation = sin(modelPosition.x * uBigWavesFrequency.x) * uBigWavesElevation;
     modelPosition.y += elevation;
 
     vec4 viewPosition = viewMatrix * modelPosition;
