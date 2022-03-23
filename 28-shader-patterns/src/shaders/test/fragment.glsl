6eaf6e9e8d8d18a726501bd6131ee7d7f93d0047
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 09:20:55 -0500

    Combine previous two to draw a grid with random values

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -92,8 +92,15 @@ void main()
     // // Pattern 22
     // float strength = floor(vUv.x * 10.0) / 10.0 * floor(vUv.y * 10.0) / 10.0;
 
-    // Pattern 23
-    float strength = random(vUv);
+    // // Pattern 23
+    // float strength = random(vUv);
+
+    // Pattern 24
+    vec2 gridUv = vec2(
+        floor(vUv.x * 10.0) / 10.0,
+        floor(vUv.y * 10.0) / 10.0
+    );
+    float strength = random(gridUv);
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
