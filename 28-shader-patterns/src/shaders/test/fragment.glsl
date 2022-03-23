4c6b7381098489a07637e4c26f4b5c20294cc859
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 10:18:36 -0500

    Draw circle in center of plane

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -138,11 +138,14 @@ void main()
     // float strength = 0.15 / (distance(vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
     // strength *= 0.15 / (distance(vec2(vUv.y, (vUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
 
-    // Pattern 32
-    vec2 rotatedUv = rotate(vUv, PI * 0.25, vec2(0.5));
+    // // Pattern 32
+    // vec2 rotatedUv = rotate(vUv, PI * 0.25, vec2(0.5));
 
-    float strength = 0.15 / (distance(vec2(rotatedUv.x, (rotatedUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
-    strength *= 0.15 / (distance(vec2(rotatedUv.y, (rotatedUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
+    // float strength = 0.15 / (distance(vec2(rotatedUv.x, (rotatedUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
+    // strength *= 0.15 / (distance(vec2(rotatedUv.y, (rotatedUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
+
+    // Pattern 33
+    float strength = step(0.5, distance(vUv, vec2(0.5)) + 0.25);
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
