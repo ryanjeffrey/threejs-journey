bf9d2f50edc766c0c1244e3303a3b89db37e2c8b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 16:43:04 -0500

    Change background color to mint

--- 29-raging-sea/src/script.js
+++ 29-raging-sea/src/script.js
@@ -118,6 +118,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor('#F5FFFA')
 
 /**
  * Animate
