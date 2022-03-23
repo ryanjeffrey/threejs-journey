09a8405b3de15c262b00e5f27dde9107129b25a7
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 11:06:36 -0500

    Draw funky waves pattern

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -245,8 +245,11 @@ void main()
     // // Pattern 47
     // float strength = step(0.0, cnoise(vUv * 10.0));
 
-    // Pattern 48
-    float strength = 1.0 - abs(cnoise(vUv * 10.0));
+    // // Pattern 48
+    // float strength = 1.0 - abs(cnoise(vUv * 10.0));
+
+    // Pattern 49
+    float strength = sin(cnoise(vUv * 10.0) * 20.0);
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
