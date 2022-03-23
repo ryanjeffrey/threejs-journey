61e292e40f410962716f2b5bc3e2d9029e601860
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 14:52:06 -0500

    Apply the colors to the waves as a test

--- 29-raging-sea/src/shaders/water/fragment.glsl
+++ 29-raging-sea/src/shaders/water/fragment.glsl
@@ -3,5 +3,6 @@ uniform vec3 uSurfaceColor;
 
 void main()
 {
-    gl_FragColor = vec4(0.5, 0.8, 1.0, 1.0);
+    // gl_FragColor = vec4(uDepthColor, 1.0);
+    gl_FragColor = vec4(uSurfaceColor, 1.0);
 }
