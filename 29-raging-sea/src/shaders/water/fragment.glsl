49c2052d7456eb210b147a81a30827c2ac4cbdcd
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 14:59:19 -0500

    Retrieve the varying and create color mix

--- 29-raging-sea/src/shaders/water/fragment.glsl
+++ 29-raging-sea/src/shaders/water/fragment.glsl
@@ -1,8 +1,10 @@
 uniform vec3 uDepthColor;
 uniform vec3 uSurfaceColor;
 
+varying float vElevation;
+
 void main()
 {
-    // gl_FragColor = vec4(uDepthColor, 1.0);
-    gl_FragColor = vec4(uSurfaceColor, 1.0);
+    vec3 color = mix(uDepthColor, uSurfaceColor, vElevation);
+    gl_FragColor = vec4(color, 1.0);
 }
