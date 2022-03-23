e586cae9be9085f8f567da03808f1ec5e3bd76cd
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 15:54:51 -0500

    Apply uniforms to for loop

--- 29-raging-sea/src/shaders/water/vertex.glsl
+++ 29-raging-sea/src/shaders/water/vertex.glsl
@@ -3,6 +3,11 @@ uniform float uBigWavesElevation;
 uniform vec2 uBigWavesFrequency;
 uniform float uBigWavesSpeed;
 
+uniform float uSmallWavesElevation;
+uniform float uSmallWavesFrequency;
+uniform float uSmallWavesSpeed;
+uniform float uSmallIterations;
+
 varying float vElevation;
 
 // Classic Perlin 3D Noise 
@@ -99,12 +104,10 @@ void main()
                       sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) * 
                       uBigWavesElevation;
 
-    for(float i = 1.0; i <= 3.0; i++)
+    for(float i = 1.0; i <= uSmallIterations; i++)
     {
-        elevation -= abs(cnoise(vec3(modelPosition.xz * 3.0 * i, uTime * 0.2)) * 0.15 / i);
-    }
-
-    
+        elevation -= abs(cnoise(vec3(modelPosition.xz * uSmallWavesFrequency * i, uTime * uSmallWavesSpeed)) * uSmallWavesElevation / i);
+    }    
                       
     modelPosition.y += elevation;
 
