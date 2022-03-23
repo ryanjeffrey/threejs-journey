2e4beed32269bd6bcdd5b5302487225c823eb327
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-23 13:30:10 -0500

    Create a uTime uniform

--- 29-raging-sea/src/script.js
+++ 29-raging-sea/src/script.js
@@ -29,6 +29,7 @@ const waterMaterial = new THREE.ShaderMaterial({
     fragmentShader: waterFragmentShader,
     uniforms:
     {
+        uTime: { value: 0 },
         uBigWavesElevation: { value: 0.2 },
         uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) }
     }
