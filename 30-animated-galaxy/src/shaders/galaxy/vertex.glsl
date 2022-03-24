8695ea19d16320cb037744829e23c49a534538c1
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-24 10:08:39 -0500

    Retrieve the attribute in the vertex shader and multiply it by the uSize

--- 30-animated-galaxy/src/shaders/galaxy/vertex.glsl
+++ 30-animated-galaxy/src/shaders/galaxy/vertex.glsl
@@ -1,5 +1,7 @@
 uniform float uSize;
 
+attribute float aScale;
+
 void main()
 {
     /**
@@ -13,5 +15,5 @@ void main()
     /**
     * Size
     */
-    gl_PointSize = uSize;
+    gl_PointSize = uSize * aScale;
 }
