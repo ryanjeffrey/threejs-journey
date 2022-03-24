ef56fe16df01d16a86359ec444c6825b400f3ca3
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-24 11:17:06 -0500

    Apply the angleOffset to the base angle

--- 30-animated-galaxy/src/shaders/galaxy/vertex.glsl
+++ 30-animated-galaxy/src/shaders/galaxy/vertex.glsl
@@ -16,6 +16,7 @@ void main()
     float angle = atan(modelPosition.x, modelPosition.z);
     float distanceToCenter = length(modelPosition.xz);
     float angleOffset = (1.0 / distanceToCenter) * uTime * 0.2;
+    angle += angleOffset;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
