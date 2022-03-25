fa8df5b1528fa8eda2344cf893c2e9f3f9dee088
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-25 13:24:50 -0500

    Import RenderPass

--- 32-post-processing/src/script.js
+++ 32-post-processing/src/script.js
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
 import * as dat from 'lil-gui'
 
 /**
