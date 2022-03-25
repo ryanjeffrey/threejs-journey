8be0750f460ee115ce2eeb2d2352dbfbb0b1d09c
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-25 09:47:54 -0500

    Retrieve the uTime uniform

--- 31-modified-materials/src/script.js
+++ 31-modified-materials/src/script.js
@@ -80,6 +80,8 @@ material.onBeforeCompile = (shader) =>
         `
             #include <common>
 
+            uniform float uTime;
+
             mat2 get2dRotateMatrix(float _angle)
             {
                 return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
