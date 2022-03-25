8f2241859ba8c39bb992620a20804f3501b32e89
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-25 09:45:04 -0500

    Make the angle vary according to the elevation

--- 31-modified-materials/src/script.js
+++ 31-modified-materials/src/script.js
@@ -91,7 +91,7 @@ material.onBeforeCompile = (shader) =>
         `
             #include <begin_vertex>
 
-            float angle = 0.3;
+            float angle = position.y * 0.9;
             mat2 rotateMatrix = get2dRotateMatrix(angle);
 
             transformed.xz = transformed.xz * rotateMatrix;
