12717f1ed5135434be0cac393b8ffae4bffb65f6
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-25 10:28:21 -0500

    Move variable declarations higher in the injection to avoid double declaration

--- 31-modified-materials/src/script.js
+++ 31-modified-materials/src/script.js
@@ -102,15 +102,15 @@ material.onBeforeCompile = (shader) =>
         '#include <beginnormal_vertex>',
         `
             #include <beginnormal_vertex>
+
+            float angle = (position.y + uTime) * 0.9;
+            mat2 rotateMatrix = get2dRotateMatrix(angle);
         `
     )
     shader.vertexShader = shader.vertexShader.replace(
         '#include <begin_vertex>', 
         `
             #include <begin_vertex>
-
-            float angle = (position.y + uTime) * 0.9;
-            mat2 rotateMatrix = get2dRotateMatrix(angle);
 
             transformed.xz = transformed.xz * rotateMatrix;
         `
