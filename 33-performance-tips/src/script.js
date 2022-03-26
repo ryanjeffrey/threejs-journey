42f56d08b5f52b613f07cb6bf5653b053582ae2a
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-26 11:53:38 -0500

    Import and instantiate Stats

--- 33-performance-tips/src/script.js
+++ 33-performance-tips/src/script.js
@@ -1,6 +1,12 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import Stats from 'stats.js'
+
+/**
+ * Stats
+ */
+const stats = new Stats()
 
 /**
  * Base
