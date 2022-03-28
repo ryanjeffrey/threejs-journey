ff4cd8fb57c0c2f8a1d66cfba374e03f59146695
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-28 09:09:56 -0500

    Import BufferGeometryUtils

--- 33-performance-tips/src/script.js
+++ 33-performance-tips/src/script.js
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import Stats from 'stats.js'
+import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js'
 
 /**
  * Stats
@@ -191,29 +192,11 @@ floor.receiveShadow = true
 // renderer.shadowMap.autoUpdate = false
 // renderer.shadowMap.needsUpdate = true
 
-// Tip 18
+// // Tip 18
+// const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
 
-const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
-
-for(let i = 0; i < 50; i++)
-{
-    const material = new THREE.MeshNormalMaterial()
-    
-    const mesh = new THREE.Mesh(geometry, material)
-    mesh.position.x = (Math.random() - 0.5) * 10
-    mesh.position.y = (Math.random() - 0.5) * 10
-    mesh.position.z = (Math.random() - 0.5) * 10
-    mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2
-    mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2
-
-    scene.add(mesh)
-}
-
-// // Tip 19
 // for(let i = 0; i < 50; i++)
 // {
-//     const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
-
 //     const material = new THREE.MeshNormalMaterial()
     
 //     const mesh = new THREE.Mesh(geometry, material)
@@ -225,6 +208,23 @@ for(let i = 0; i < 50; i++)
 
 //     scene.add(mesh)
 // }
+
+// Tip 19
+for(let i = 0; i < 50; i++)
+{
+    const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+
+    const material = new THREE.MeshNormalMaterial()
+    
+    const mesh = new THREE.Mesh(geometry, material)
+    mesh.position.x = (Math.random() - 0.5) * 10
+    mesh.position.y = (Math.random() - 0.5) * 10
+    mesh.position.z = (Math.random() - 0.5) * 10
+    mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2
+    mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2
+
+    scene.add(mesh)
+}
 
 // // Tip 20
 // const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
