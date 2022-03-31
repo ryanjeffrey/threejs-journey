31a80869e39135f2f4b65a4e5bb3843da12651b2
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-31 16:48:30 -0500

    Slow down the animation

--- 39-adding-details-to-the-scene/src/shaders/portal/fragment.glsl
+++ 39-adding-details-to-the-scene/src/shaders/portal/fragment.glsl
@@ -82,10 +82,10 @@ varying vec2 vUv;
 void main ()
 {
     // Displace the UV
-    vec2 displacedUv = vUv + cnoise(vec3(vUv * 5.0, uTime));
+    vec2 displacedUv = vUv + cnoise(vec3(vUv * 5.0, uTime * 0.1));
 
     // Perlin noise
-    float strength = cnoise(vec3(displacedUv * 5.0, uTime));
+    float strength = cnoise(vec3(displacedUv * 5.0, uTime * 0.2));
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
