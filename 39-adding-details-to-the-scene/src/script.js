0ac2a2854197277f90ff566f598c6ed43bb0681b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-31 16:07:34 -0500

    Create a uTime uniform and update it in the tick function

--- 39-adding-details-to-the-scene/src/script.js
+++ 39-adding-details-to-the-scene/src/script.js
@@ -104,6 +104,7 @@ const firefliesMaterial = new THREE.ShaderMaterial({
     transparent: true,
     uniforms:
     {
+        uTime: { value: 0 },
         uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) },
         uSize: { value: 100 }
     },
@@ -185,6 +186,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update materials
+    firefliesMaterial.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
