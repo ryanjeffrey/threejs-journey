b5f749e571b57b2869b9bd45ed2a718347463112
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-31 16:52:17 -0500

    Get the distances to the center and apply it to the FragColor

--- 39-adding-details-to-the-scene/src/shaders/portal/fragment.glsl
+++ 39-adding-details-to-the-scene/src/shaders/portal/fragment.glsl
@@ -87,5 +87,8 @@ void main ()
     // Perlin noise
     float strength = cnoise(vec3(displacedUv * 5.0, uTime * 0.2));
 
-    gl_FragColor = vec4(strength, strength, strength, 1.0);
+    // Outer glow
+    float outerGlow = distance(vUv, vec2(0.5));
+
+    gl_FragColor = vec4(outerGlow, outerGlow, outerGlow, 1.0);
 }
