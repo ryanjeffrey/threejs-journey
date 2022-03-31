70c212ccc98a10495f3607025466d17f8d79097b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-03-31 15:28:42 -0500

    Apply the distanceToCenter variable to the alpha

--- 39-adding-details-to-the-scene/src/shaders/fireflies/fragment.glsl
+++ 39-adding-details-to-the-scene/src/shaders/fireflies/fragment.glsl
@@ -1,5 +1,6 @@
 void main ()
 {
     float distanceToCenter = distance(gl_PointCoord, vec2(0.5));
-    gl_FragColor = vec4(gl_PointCoord, 1.0, 1.0);
+    
+    gl_FragColor = vec4(1.0, 1.0, 1.0, distanceToCenter);
 }
